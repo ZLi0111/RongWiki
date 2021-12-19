4a38fdb50f17bde9a523f2da683146923a5a949e
Zhengrong Li <zhengrongli1003@gmail.com>
2021-12-19 02:21:49 -0800

    2-2 init

---
+++ src/main/java/com/rong/rongwiki/RongWikiApplication.java
@@ -0,0 +1,13 @@
+package com.rong.rongwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RongWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RongWikiApplication.class, args);
+    }
+
+}
