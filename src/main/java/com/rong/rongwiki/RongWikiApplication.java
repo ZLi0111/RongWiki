baa104d5a3cf41d22e78dca7b8c38e2393258610
Zhengrong Li <zhengrongli1003@gmail.com>
2021-12-19 12:36:11 -0800

    2-5 修改启动文案

--- src/main/java/com/rong/rongwiki/RongWikiApplication.java
+++ src/main/java/com/rong/rongwiki/RongWikiApplication.java
@@ -1,13 +1,19 @@
 package com.rong.rongwiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class RongWikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(RongWikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(RongWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(RongWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
