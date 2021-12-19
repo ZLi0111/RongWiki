827d0236c0e4a6d2042f9162f680a19eba1c7806
Zhengrong Li <zhengrongli1003@gmail.com>
2021-12-19 12:51:58 -0800

    2-6 Add Hello World interface address: http://127.0.0.1:8080/hello

--- src/main/java/com/rong/rongwiki/config/RongWikiApplication.java
+++ src/main/java/com/rong/rongwiki/config/RongWikiApplication.java
@@ -1,11 +1,13 @@
-package com.rong.rongwiki;
+package com.rong.rongwiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.rong")
 @SpringBootApplication
 public class RongWikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(RongWikiApplication.class);
