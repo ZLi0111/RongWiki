ed8b1c39e25bc0e4c547548190a686cabec651f6
Zhengrong Li <zhengrongli1003@gmail.com>
2021-12-20 12:08:48 -0800

    3-1 add all.sql and add mybatis my sql into dependency

--- src/main/java/com/rong/rongwiki/controller/TestController.java
+++ src/main/java/com/rong/rongwiki/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.rong.rongwiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
+    }
+
+    @RequestMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World!" + name;
     }
 }
