d5fa9223459f6af2f94f65b609690ceadd08a7c4
Zhengrong Li <zhengrongli1003@gmail.com>
2021-12-20 22:06:17 -0800

    3-6 common respone

--- src/main/java/com/rong/rongwiki/controller/EbookController.java
+++ src/main/java/com/rong/rongwiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.rong.rongwiki.controller;
 
 import com.rong.rongwiki.domain.Ebook;
+import com.rong.rongwiki.resp.CommonResp;
 import com.rong.rongwiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,10 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
